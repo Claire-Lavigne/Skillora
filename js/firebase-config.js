a4c38641b7bce817530459a91d8f1967e077cef2
Claire Lavigne <lavigneclaire81@gmail.com>
2026-09-25 12:57:00 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,11 @@
-// Remplace les valeurs ci-dessous par celles de ton projet Firebase.
 // Firebase Console > Paramètres du projet > Tes applications > Application Web
 
 export const firebaseConfig = {
-  apiKey: "REMPLACE_PAR_TA_CLE_API",
-  authDomain: "REMPLACE_PAR_TON_AUTH_DOMAIN",
-  projectId: "REMPLACE_PAR_TON_PROJECT_ID",
-  storageBucket: "REMPLACE_PAR_TON_STORAGE_BUCKET",
-  messagingSenderId: "REMPLACE_PAR_TON_MESSAGING_SENDER_ID",
-  appId: "REMPLACE_PAR_TON_APP_ID"
+  apiKey: "AIzaSyA1aYw7_T3V7kEMGZBNfRHW53O1Jjy_5Ps",
+  authDomain: "skillora-2a8fa.firebaseapp.com",
+  projectId: "skillora-2a8fa",
+  storageBucket: "skillora-2a8fa.firebasestorage.app",
+  messagingSenderId: "172271272312",
+  appId: "1:172271272312:web:5c00f3c4b285a46d41c88d",
+  measurementId: "G-R0QYHDWW9Z"
 };
